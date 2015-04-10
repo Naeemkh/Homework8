1647a699b57f7e9f8465df2d0ae5e7b1900ed2a8
nayeemnoman173 <nayeemnoman@gmail.com>
2015-04-10 12:28:38 -0500

    Update main.cpp
    Prepared a function for multiple input file names

--- main.cpp
+++ main.cpp
@@ -4,4 +4,43 @@
  * Homework 8 
  */
 
+#include <iostream>
+#include <list>
 
+using namespace std;
+
+
+//A FUNCTION THAT CAN PRINT THE INPUT FILE NAMES
+
+void get_file_names (const char* filename) { 
+	
+	cout << filename << endl;
+} 
+
+
+
+//MAIN FUNCTION STARTS HERE
+
+int main(int argc, char** argv) {
+	
+	int num_eqs = argc-1;                 //Declaring & Getting number of earthquakes
+	list<char*> file_names;               //Declaring input file names
+	
+	for (int i = 1; i < argc; ++i) {
+		get_file_names (argv[i]);
+		file_names.push_back(argv[i]);    //Storing file names in a list
+		
+	}
+	
+	
+	// Now each file name like file_names[1] or file_names[2] ..... can work as individual input file
+	// Also we got a variable of number of earthquakes as num_eqs
+	
+	//.......
+	
+	// Here We can process each job with each file name .. file_names[].
+	
+    //..........
+	
+	
+}
