345be77fdfd9b88e65ed3594d28acfa10d5f8df0
nayeemnoman173 <nayeemnoman@gmail.com>
2015-04-14 23:13:19 -0500

    Network & station name check functions

--- station.cpp
+++ station.cpp
@@ -1,6 +1,6 @@
 #include "station.h"
 
-Network_Code string_to_Network_Code(string s) {
+Network_Code Event::string_to_Network_Code(string s) {
 
 	string ss = uppercase(s);
 
@@ -15,7 +15,32 @@ Network_Code string_to_Network_Code(string s) {
 
 }
 
-string Band_Type_to_string(Band_Type c) {
+string Event::Network_Code_to_string(Network_Code c) {
+	switch (c) {
+	case CE: return "CE";
+	case CI: return "CI";
+	case FA: return "FA";
+	case NP: return "NP";
+	case WR: return "WR";
+	}
+	// It should never get here!!
+	exit (EXIT_FAILURE);
+}
+
+Band_Type Event::string_to_Band_Type(string s) {
+
+	string ss = uppercase(s);
+
+	if (ss == "LONG-PERIOD") return Longperiod;
+	if (ss == "SHORT-PERIOD") return Shortperiod;
+	if (ss == "BROADBAND") return Broadband;
+
+	// It should never get here!!
+	exit (EXIT_FAILURE);
+
+}
+
+string Event::Band_Type_to_string(Band_Type c) {
 
 	switch (c) {
 	case Longperiod: return "L";
@@ -26,6 +51,88 @@ string Band_Type_to_string(Band_Type c) {
 
 	// It should never get here!!
 	exit (EXIT_FAILURE);
+}
+
+bool Event::set_nt_name(Event db[MAXSIZE], string nt_name1, int total_entry,
+		ofstream& errorfile) {
+
+	string ss = uppercase(nt_name1);
+	if (((ss == "CE") || (ss == "CI") || (ss == "FA") || (ss == "NP")
+			|| (ss == "WR")) && (ss == nt_name1)) {
+		nt_name = string_to_Network_Code(nt_name1);
+
+		return 0;
+	} else {
+
+		stringstream ks;
+		string kk;
+		ks << total_entry;
+		ks >> kk;
+		print_output(errorfile, cout, "Entry # ");
+		print_output(errorfile, cout, kk);
+		print_output(errorfile, cout, " Ignored. ");
+		print_output(errorfile, cout, "Invalid Network. \n");
+
+		return 1;
+	}
+}
+
+Network_Code Event::get_nt_name(Event db[MAXSIZE]) {
+
+	return nt_name;
+}
+
+bool Event::set_st_name(Event db[MAXSIZE], string st_name1, int total_entry,
+		ofstream& errorfile) {
+
+	bool st_flag = 0;
+	string ss = uppercase(st_name1);
+	int sn = st_name1.size();
+
+	stringstream st_sst;
+	int st_in;
+	string st_string;
+
+	st_sst << st_name1;
+	st_sst >> st_in;
+	st_sst << st_in;
+	st_sst >> st_string;
+
+	if (sn == 3 && ss == st_name1 && st_string == "") {
+		st_flag = 1;
+	}
+	if (sn == 5) {
+		st_flag = 1;
+		for (int i = 0; i == 4; i++) {
+			if (isdigit(st_name1[i] == 0)) {
+				st_flag = 0;
+			}
+		}
+	}
+
+	if (st_flag == 1) {
+		st_name = st_name1;
+
+		return 0;
+
+	} else {
+		stringstream ks;
+		string kk;
+		ks << total_entry;
+		ks >> kk;
+
+		print_output(errorfile, cout, "Entry # ");
+		print_output(errorfile, cout, kk);
+		print_output(errorfile, cout, " Ignored. ");
+		print_output(errorfile, cout, "Invalid station code. \n");
+
+		return 1;
+
+	}
+}
+
+string Event::get_st_name(Event db[MAXSIZE]) {
+	return st_name;
 }
 
 bool Event::set_Ins_type(Event db[MAXSIZE], string Ins_type1, int total_entry, ofstream& errorfile) {
@@ -118,32 +225,4 @@ bool Event::set_orientation(Event db[MAXSIZE], string orientation1, int total_en
 
 string Event::get_orientation(Event db[MAXSIZE]) {
 	return orientation;
-}
-
-bool Event::set_nt_name(Event db[MAXSIZE], string nt_name1, int total_entry, ofstream& errorfile) {
-
-	string ss = uppercase(nt_name1);
-	if (((ss == "CE") || (ss == "CI") || (ss == "FA") || (ss == "NP")
-		|| (ss == "WR")) && (ss == nt_name1)) {
-			nt_name = string_to_Network_Code(nt_name1);
-
-			return 0;
-	} else {
-
-		stringstream ks;
-		string kk;
-		ks << total_entry;
-		ks >> kk;
-		print_output(errorfile, cout, "Entry # ");
-		print_output(errorfile, cout, kk);
-		print_output(errorfile, cout, " Ignored. ");
-		print_output(errorfile, cout, "Invalid Network. \n");
-
-		return 1;
-	}
-}
-
-Network_Code Event::get_nt_name(Event db[MAXSIZE]) {
-
-	return nt_name;
 }
